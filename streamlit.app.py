a8ed947e95df75160b468e9fc6501d7ff5be4a30
Rahulgoyal1991 <137926055+Rahulgoyal1991@users.noreply.github.com>
2023-06-30 00:09:35 +0530

    Update streamlit.app.py
    Emojis

--- streamlit.app.py
+++ streamlit.app.py
@@ -2,7 +2,8 @@ import streamlit
 
 streamlit.title('my parents new healthy dine')
 
-streamlit.header(' 🥣Breakfast Favourites')
-streamlit.text(' 🥗 Omega 3 & Blueberry Oatmeal')
-streamlit.text(' 🐔 Kale, Spinach & Rocket Smoothie')
-streamlit.text(' 🥑🍞 Hard-Boiled Free-Range Egg')
+streamlit.header(' Breakfast Favourites')
+streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
